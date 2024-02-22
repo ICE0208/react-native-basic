53c3988020009a22bc91a9cbec6b2146d02459b0
ICE0208 <doky3307@naver.com>
2024-02-23 00:43:59 +0900

    Touchables

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "./colors";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={work}
+          activeOpacity={0.8}
+        >
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.gray }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={travel}
+          activeOpacity={0.8}
+        >
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.gray,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +43,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 40,
+  },
+  header: {
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    marginTop: 100,
+    columnGap: 30,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
 });
---
+++ todo-list/README.md
@@ -0,0 +1,17 @@
+## Touchable
+
+### TouchableOpacity
+
+누르면 래핑된 View의 opacity가 감소하여 흐리게 표시된다.
+
+### TouchableHighlight
+
+누르면 래핑된 View에 background를 표시한다.
+
+### TouchableWithoutFeedback
+
+터치는 필요하지만 어떠한 피드백도 필요하지 않을 때 사용한다.
+
+### Pressable
+
+Pressable은 정의된 자식에 대한 다양한 Press 상호 작용 단계를 감지할 수 있는 핵심 컴포넌트이다.
